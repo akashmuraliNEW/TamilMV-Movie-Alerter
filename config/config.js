63f432900076a2503e8950c7673776975026b795
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2024-01-04 18:17:31 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ module.exports = {
 		'accept-encoding': '*',
 		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
 	},
-	dbUrl: process.env.dbUrl,
-	telegramAPIKey: process.env.telegramAPIKey,
-	telegramChatId: process.env.telegramChatId,
-	cronJobIntervalInMinutes: process.env.cronJobIntervalInMinutes
+	dbUrl: 'mongodb+srv://tamilmv:tamilmv@cluster0.fy3daai.mongodb.net/?retryWrites=true&w=majority',
+	telegramAPIKey: '2003350455:AAH0J2h2lm6vUeUrSjustbl8Z1jiQi1IyVY',
+	te1legramChatId: '-1002132140229',
+	cronJobIntervalInMinutes: '1'
 };
